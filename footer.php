114c56b788e0b4b8ec580c5e6784e40f4c598fe4
JelenaJelenkovic <57746585+JelenaJelenkovic@users.noreply.github.com>
2022-04-20 23:23:11 +0200

    v1
    inicijalizacija projekta

---
+++ footer.php
@@ -0,0 +1,16 @@
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-4">
+                
+            </div>
+            <div class="col-md-4 col-sm-4">
+                
+            </div>
+            <div class="col-md-4 col-sm-4 text-right">
+                
+            </div>
+        </div>
+    </div>
+</div>
+
